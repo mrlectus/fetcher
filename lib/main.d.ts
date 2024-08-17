575fcbf5212f66d3a6d05a4c4ae19a3b63e80ef0
mrlectus <lilbrown3000@gmail.com>
2024-08-17 12:21:30 +0100

    forget build

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -4,9 +4,10 @@ export declare const fetcher: <T>({ baseUrl, token, }: {
     baseUrl: ValidUrl;
     token?: string;
 }) => Promise<T>;
-export declare const mutation: <T>({ baseUrl, token, method, }: {
+export declare const mutation: <T>({ baseUrl, token, method, payload, }: {
     baseUrl: ValidUrl;
     token?: string;
     method: Mutation;
+    payload: RequestInit["body"];
 }) => Promise<T>;
 export {};
